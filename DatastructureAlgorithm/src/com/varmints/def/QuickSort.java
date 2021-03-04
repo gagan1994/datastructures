6f967a4743bf6c7f9e3088f53e4ac4f5377bcae5
Gagan Patil <gaganpatil@IT-Admins-MacBook-Pro.local>
2021-03-04 21:28:07 +0530

    added project

---
+++ DatastructureAlgorithm/src/com/varmints/def/QuickSort.java
@@ -0,0 +1,52 @@
+package com.varmints.def;
+
+
+import java.util.Random;
+
+public class QuickSort {
+
+    public void mainTest() {
+        int[] array = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+        System.out.println("Array list before: ");
+        printArray(array);
+        quickSort(0, array.length - 1, array);
+        System.out.println("Array list after: ");
+        printArray(array);
+    }
+
+    private void quickSort(int low, int high, int[] array) {
+        if (low < high) {
+            int piviot = partition(low, high, array);
+            quickSort(piviot + 1, high, array);
+            quickSort(low, piviot - 1, array);
+        }
+    }
+
+    private int partition(int low, int high, int[] array) {
+        int pi = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j] < pi) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        return i + 1;
+    }
+
+   public static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
---
+++ DatastructureAlgorithm/src/com/varmints/def/StackOperations.java
@@ -0,0 +1,57 @@
+package com.varmints.def;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+public class StackOperations {
+    public void reverseStringUsingStack() {
+        String str = "asdfghjkl";
+        char[] chars = str.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < chars.length; i++) {
+            stack.push(chars[i]);
+        }
+        String rev = "";
+        int size = stack.size();
+        for (int i = 0; i < size; i++) {
+            rev = rev + stack.pop();
+        }
+        System.out.println("original: " + str);
+        System.out.println("reversed: " + rev);
+    }
+
+    public void balancedParanthsis() {
+        String input="[()]{}{[()()]()}";
+//        String input = "[(])";
+        char[] chars = input.toCharArray();
+        Stack stack = new Stack();
+        for (int i = 0; i < chars.length; i++) {
+            char x = chars[i];
+            if (x == '{' || x == '[' || x == '(') {
+                stack.push(chars[i]);
+                continue;
+            }
+            assert !stack.isEmpty();
+            switch (x) {
+                case ')':
+                    if (stack.get(stack.size()-1).equals('(')) {
+                        stack.pop();
+                    }
+                    break;
+                case '}':
+                    if (stack.get(stack.size()-1).equals('{')) {
+                        stack.pop();
+                    }
+                    break;
+                case ']':
+                    if (stack.get(stack.size()-1).equals('[')) {
+                        stack.pop();
+                    }
+                    break;
+            }
+
+        }
+        assert stack.isEmpty();
+    }
+}
